db91c6ab904738e9ff5cad480f62344a66a6773c
Alex Nichol <unixpickle@gmail.com>
2018-10-28 12:43:55 -0400

    generate results markdown

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # depth-to-rgb
 
 The problem is simple: encode a 16-bit depth image as a 24-bit RGB image, such that applying compression to the RGB image doesn't totally destroy the 16-bit depth reconstructions.
+
+# Results
+
+Here are the results of running various algorithms on the test image(s):
+
+| Name | 10% quality | 50% quality | 100% quality |
+|:----:|:-----------:|:-----------:|:------------:|
+| grayscale | 914.77 | 292.61 | 106.24 |
+| wrapbit | 248.71 | 90.67 | 43.85 |
--- run_benchmarks.py
+++ run_benchmarks.py
@@ -2,6 +2,8 @@
 Benchmark all of the algorithms.
 """
 
+import numpy as np
+
 import depth_to_rgb
 
 TRANSCODERS = {
@@ -11,10 +13,25 @@ TRANSCODERS = {
 
 
 def main():
+    all_results = {}
     for name, coder in TRANSCODERS.items():
         print('Benchmarking %s...' % name)
         results = depth_to_rgb.benchmark_transcoder(coder)
         depth_to_rgb.print_benchmark_results(results)
+        all_results[name] = results
+    save_markdown_results('RESULTS.md', all_results)
+
+
+def save_markdown_results(filename, all_results):
+    output_data = ('| Name | 10% quality | 50% quality | 100% quality |\n' +
+                   '|:----:|:-----------:|:-----------:|:------------:|\n')
+    for name, results in all_results.items():
+        def _mean(quality):
+            return np.mean([x[quality] for x in results.values()])
+        output_data += ('| %s | %.2f | %.2f | %.2f |\n' %
+                        (name, _mean(10), _mean(50), _mean(100)))
+    with open(filename, 'wt+') as out_file:
+        out_file.write(output_data)
 
 
 if __name__ == '__main__':
